a55e8dc0225d93ddff54b8a42c031d8a3b0b2130
Mr-Yellow <mr-yellow@mr-yellow.com>
2022-02-04 10:38:56 +1000

    devops: Updated build

--- locks.gulp.js
+++ locks.gulp.js
@@ -1,9 +1,12 @@
+const debug = require('debug')('doop:lock');
+debug('Init gulp lock');
+
 const gulp = require('gulp');
 
-gulp.task('load:app.lock', ['load:app.db'], ()=>
+gulp.task('load:app.lock', gulp.series('load:app.db'), ()=>
 	app.emit('locks')
 );
 
-gulp.task('locks.clear', ['load:app.lock'], ()=>
+gulp.task('locks.clear', gulp.series('load:app.lock'), ()=>
 	app.lock.clear()
 );
